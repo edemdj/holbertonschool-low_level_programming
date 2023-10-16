e9a65ec67c3506c74d2d37950f660fc9b04cb43f
edemdj <edemdjossou1997@gmail.com>
2023-10-16 02:15:33 -0700

    hello puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,16 @@
 #!/bin/bash
-gcc prinf "Programming is like building a multilingual puzzle
+#include <stdio.h>
+#include <string.h>
+/**
+ *  *main - prints exactly "Programming is like building a multilingual puzzle,
+ *   *followed by a new line
+ *    *
+ *     *Return:  0 on success. Error code otherwise
+ *      */
+int main(void)
+{
+        char str1[] = "\"Programming is like building a multilingual puzzle";
+        puts(str1);
+        return (0);
+}
+~
