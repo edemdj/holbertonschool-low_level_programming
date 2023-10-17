b427796cfaff54f132133c33170816b05bda2e36
edemdj <edemdjossou1997@gmail.com>
2023-10-17 13:11:23 -0700

    types de variables

---
+++ hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	printf("size of a char:%d byte(s)∖n" , sizeof(char));
+	printf("size of an int: %d byte(s)∖n" , sizeof(int));
+	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
+	printf("size of a long int: %d byte(s)∖n",sizeof(long long int));
+	printf("size of a float: %dbytes(s)∖n" ,sizeof(float));
+	return (0);
+}
