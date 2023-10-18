5293bfcfdd3429b44ab912e259c3bd73b658e3d2
edemdj <edemdjossou1997@gmail.com>
2023-10-18 06:52:08 -0700

    afficher l'alphabet à l'envers

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -13,9 +13,6 @@ int main(void)
 	for (letter = 'z'; letter <= 'a'; letter++)
 		putchar(letter);
 
-	for (letter = 'A'; letter <= 'Z'; letter++)
-		putchar(letter);
-
 	putchar ('\n');
 
 	return (0);
