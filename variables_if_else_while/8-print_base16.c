07b3e79a7f8a83f00bf445b4fb83fb0f76a57367
edemdj <edemdjossou1997@gmail.com>
2023-10-18 08:01:26 -0700

    afficher les nombres de base 16

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - prints all the numbers of base 16 in lowercase
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char numbers;
+	char letter;
+
+	for (numbers = '0'; letter <= '9'; numbers++)
+		putchar(numbers);
+
+	for (letter = 'a'; letter <= 'f'; letter++)
+		putchar(letter)
+
+	putchar ('\n');
+
+	return (0);
+}
