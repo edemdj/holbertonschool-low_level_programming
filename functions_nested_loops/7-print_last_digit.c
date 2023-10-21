979e5ac6a121f2705da599d7cb06a2ed7a6fb55d
edemdj <edemdjossou1997@gmail.com>
2023-10-21 02:28:54 -0700

    afficher la valeur absolue

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_last_digit - function that prints the last digit of a number
+ *@n: number
+ * Return: value of n
+ */
+int print_last_digit(int n)
+{
+	int l;
+
+	l = n % 10;
+	if (l < 0)
+	{
+		_putchar(-l + 48);
+		return (-l);
+	}
+	else
+	{
+		_putchar(l + 48);
+		return (l);
+	}
+}
