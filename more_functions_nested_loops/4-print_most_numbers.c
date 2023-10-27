584c2d393175d51db7f37a45c3fc1f5bada7e917
edemdj <edemdjossou1997@gmail.com>
2023-10-27 06:11:10 -0700

    4-print_most_numbers.c

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,11 +1,9 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints numbers from 0 to 9, but 2 and 4
- * void: No Input Parameter
- *@i:number
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * print_most_numbers -  checks for checks for a digit (0 through 9).
+ *
+ * Return: Always 0.
  */
 void print_most_numbers(void)
 {
