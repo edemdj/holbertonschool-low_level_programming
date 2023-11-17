c841e988aef7d12a12d4651aaedc9eedb55a3608
edemdj <edemdjossou1997@gmail.com>
2023-11-17 01:07:03 -0800

    program that performs simple operations.

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,5 +1,14 @@
 #include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
 
+/**
+ * main - Prints the result of simple operations.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always 0.
+ */
 int main(int argc, char *argv[])
 {
 	int num1, num2;
