471f68fd665aa50d6fe7343951d12b99881e8c68
edemdj <edemdjossou1997@gmail.com>
2023-12-07 00:44:43 -0800

    function that creates a file.

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -18,7 +18,8 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (; text_content[len]; len++);
+		for (; text_content[len]; len++)
+			;
 	}
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
